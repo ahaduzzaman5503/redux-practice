457d22dfae0e8e03bb74bbd961b225425d4ccf92
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-08-19 23:51:37 +0600

    5 no video practice again

--- index.js
+++ index.js
@@ -1,70 +1,180 @@
 const {createStore} = require("redux")
-// state
-const INCREMENT = "INCREMENT";
-const DECREMENT = " DECREMENT";
-const ADD_USER = "ADD_USER"
+//state - count:0 
+// action - increment, decrement, reset
+// reducer
+// store
 
-const inisialCounterState = {
-    count: 0,
-};
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+const RESET = "RESET"
 
-const inisialUserState = {
-    users: [{name: "Ahaduzzaman Ahad"}]
+const initialState = {
+    count: 0
 }
 
-const incrementCounter = () => {
+const incrementCounterAction = () => {
     return {
         type: INCREMENT,
     }
 }
-
-const decrementCounter = () => {
+const decrementCounterAction = () => {
     return {
         type: DECREMENT,
-    };
-};
-
-const addUser = () => {
+    }
+}
+const resetCounterAction = () => {
     return {
-        type: ADD-USER,
-        payload: {name: "Mohamad"}
-    };
-};
-
-const counterReducer = (state= inisialCounterState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                count: state.count + 1,
-            }
-
-        case DECREMENT:
-            return {
-                ...state,
-                count: state.count - 1,
-            }
-
-        default:
-            state;
+        type: RESET,
     }
 }
 
-// action
+// Creting Reduicer
+
+const counterReducer = (state = initialState, action) => {
+ switch (action.type) {
+    case INCREMENT:
+        
+        return {
+            ...state,
+            count: state.count + 1,
+        }
+    case DECREMENT:
+        
+        return {
+            ...state,
+            count: state.count - 1,
+        }
+    case RESET:
+        
+        return {
+            ...state,
+            count: 0,
+        }
+ 
+    default:
+        state;
+ }
+}
 
 // store
-
 const store = createStore(counterReducer);
 
 store.subscribe(() => {
-    console.log(store.getState());
+    console.log(store.getState());;
 })
 
-// dispathch action
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(decrementCounter())
-store.dispatch(decrementCounter())
+
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(decrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(resetCounterAction());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // state
+// const INCREMENT = "INCREMENT";
+// const DECREMENT = " DECREMENT";
+// const ADD_USER = "ADD_USER"
+
+// const inisialCounterState = {
+//     count: 0,
+// };
+
+// const inisialUserState = {
+//     users: [{name: "Ahaduzzaman Ahad"}]
+// }
+
+// const incrementCounter = () => {
+//     return {
+//         type: INCREMENT,
+//     }
+// }
+
+// const decrementCounter = () => {
+//     return {
+//         type: DECREMENT,
+//     };
+// };
+
+// const addUser = () => {
+//     return {
+//         type: ADD-USER,
+//         payload: {name: "Mohamad"}
+//     };
+// };
+
+// const counterReducer = (state= inisialCounterState, action) => {
+//     switch (action.type) {
+//         case INCREMENT:
+//             return {
+//                 ...state,
+//                 count: state.count + 1,
+//             }
+
+//         case DECREMENT:
+//             return {
+//                 ...state,
+//                 count: state.count - 1,
+//             }
+
+//         default:
+//             state;
+//     }
+// }
+
+// // action
+
+// // store
+
+// const store = createStore(counterReducer);
+
+// store.subscribe(() => {
+//     console.log(store.getState());
+// })
+
+// // dispathch action
+// store.dispatch(incrementCounter())
+// store.dispatch(incrementCounter())
+// store.dispatch(incrementCounter())
+// store.dispatch(decrementCounter())
+// store.dispatch(decrementCounter())
 
 
